3906cf4d1fcba67f335a91bb079a489c0767472d
rodan888 <tsumbaluk888@gmail.com>
2017-02-12 19:11:30 +0200

    Something is going on

--- src/Advert/Advert.js
+++ src/Advert/Advert.js
@@ -1,41 +1,33 @@
 import React, { Component } from 'react';
-import './Advert.css'
+import './Advert.css';
 
-var advertArray = [
-	{
-		'pic': '',
-		'headline': 'Lorem ipsum dolor',
-		'info': 'img/unic.png',
-		'author': 'Johnny Depp',
-		'avatar': 'img/depp.png'
-	}
-];
 class Advert extends Component {
-	render() {
+	
+	render() {	
 		return (
 			<section id="advert">
 				<h2 className="s-heading">Advert</h2>
 
 				<div className="container">
 					<div className="row">
-						<div className="slider col-md-12">	
-							{this.props.advertArray.map((item,index) => 
+						<div className="col-md-12 slider" ref="slider">	
+							{this.props.items.map((item,index) => 
 
-								<div className="item">
-									<img className="unicorn-poster" src="{}" alt="unicorn">
+								<div className="item" key={index}>
+									<img className="unicorn-poster" src={item.pic} alt="unicorn"/>
 									<div className="w-info">
 										<div className="content">
-											<h4 className="t-heading"> Lorem ipsum dolor</h4>
-											
+											<h4 className="t-heading"> {item.headline}</h4>
+											{item.info}
 										</div>
 										<div className="w-author-info">
-											<img className="avatar" src="" alt="">
-											<div className="author">Johnny Depp</div>
+											<img className="avatar" src={item.avatar} alt=""/>
+											<div className="author">{item.author}</div>
 										</div>
 									</div>
 								</div>								
-								})
-							
+								)}
+
 						</div>
 					</div>
 				</div>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header/Header.js';
 import Main from './Main/Main.js';
+import Advert from './Advert/Advert.js';
 import './index.css';
 import './main.css';
+import './bootstrap/bootstrap.min.css';
 
 const menu = [
 	{
@@ -27,13 +29,29 @@ const menu = [
 		label: 'Home'
 	}	
 ];
-
+const advertArray = [
+	{
+		pic: 'img/unic.png',
+		headline: 'Lorem ipsum dolor',
+		info: 'Lorem ipsum dolor sit amet, consectetur, Lorem ipsum dolor sit amet, consectetur adipisicing elit.Esse ut eos, pariatur est, qui nihil vitae officiis non laboriosam quia. Id quos minima quam voluptas possimus error fuga corrupti magnam!',
+		author: 'Johnny Depp',
+		avatar: '../img/depp.png'
+	},
+	{
+		pic: 'img/unic.png',
+		headline: 'Lorem ipsum dolor',
+		info: 'Lorem ipsum dolor sit amet, consectetur',
+		author: 'Johnny Depp',
+		avatar: '../img/depp.png'
+	}
+];
 var AppHome = React.createClass({
   render: function() {
     return (
       <div>
         <Header items={menu}/>
         <Main />
+        <Advert items={advertArray}/>
       </div>
     )
   }
